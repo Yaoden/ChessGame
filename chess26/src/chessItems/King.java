c7a078d785628663ec038282f8ab1ce67f7ed742
Matthew Ya <matthew_ya@yahoo.com>
2016-11-08 13:20:53 -0500

    Finished castling

--- chess26/src/chessItems/King.java
+++ chess26/src/chessItems/King.java
@@ -29,8 +29,67 @@ public class King implements ChessPiece {
 	public boolean isLegal(int fstart, int rstart, int fend, int rend, ChessPiece board[][]) throws ArrayIndexOutOfBoundsException{
 		// TODO Auto-generated method stub
 		
+		//checks for castling
+		if((Math.abs(fstart - fend) == 2) && (Math.abs(rstart - rend) == 0) && (this.moves == 0)){
+			//white castling
+			if(this.white && ((board[7][0] != null && board[7][0].toString().charAt(1) == 'R' && board[7][0].getMoves() == 0) || (board[7][7] != null && board[7][7].toString().charAt(1) == 'R' && board[7][7].getMoves() == 0))){
+				
+				//short castling
+				if(fstart < fend){
+					for(int i = fstart + 1; i < 7; i++){
+						if(board[rstart][i] != null){
+							return false;
+						}
+					}
+					board[7][6] = board[7][4];
+					board[7][5] = board[7][7];
+					board[7][7] = null;
+					board[7][4] = null;
+				//long castling
+				}else{
+					for(int i = fstart - 1; i > 0; i--){
+						if(board[rstart][i] != null){
+							return false;
+						}
+					}
+					board[7][2] = board[7][4];
+					board[7][3] = board[7][0];
+					board[7][4] = null;
+					board[7][0] = null;
+				}
+			//black castling
+			}else if(!this.white && ((board[0][0] != null && board[0][0].toString().charAt(1) == 'R' && board[0][0].getMoves() == 0) || (board[0][7] != null && board[0][7].toString().charAt(1) == 'R' && board[0][7].getMoves() == 0))){
+				//short castling
+				if(fstart < fend){
+					for(int i = fstart + 1; i < 7; i++){
+						if(board[rstart][i] != null){
+							return false;
+						}
+					}
+					board[0][6] = board[0][4];
+					board[0][5] = board[0][7];
+					board[0][7] = null;
+					board[0][4] = null;
+				//long castling
+				}else{
+					for(int i = fstart - 1; i > 0; i--){
+						if(board[rstart][i] != null){
+							return false;
+						}
+					}
+					board[0][2] = board[0][4];
+					board[0][3] = board[0][0];
+					board[0][4] = null;
+					board[0][0] = null;
+				}
+			}else{
+				return false;
+			}
+			
+			return true;
+			
 		//checks for legal king move
-		if(Math.abs(fstart - fend) > 1 || Math.abs(rstart - rend) > 1){
+		}else if(Math.abs(fstart - fend) > 1 || Math.abs(rstart - rend) > 1){
 			return false;
 		}
 		
